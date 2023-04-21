cb5a4821681ae75709e232a33a03d36c157e82e3
osbrown1 <socknot@live.com>
2023-04-21 11:15:48 -0700

    include FlavorSelection and Inventory components for webpage, webpage loads properly, debugging on-click errors for buttons

--- inventory/src/components/App.js
+++ inventory/src/components/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Header from './Header';
-import FlavorSelection from './FlavorSelection';
-import Inventory from './Inventory';
+import React from "react";
+import Header from "./Header";
+import FlavorSelection from "./FlavorSelection";
+import Inventory from "./Inventory";
 
 const App = () => {
   return (
     <React.Fragment>
-      <Container>
-        <Header />
-        <Row>
-          <Col><FlavorSelection /></Col>
-          <Col><Inventory /></Col>
-        </Row>
-      </Container>
+    <Header />
+    <FlavorSelection />
+    <Inventory />
     </React.Fragment>
   );
 }
