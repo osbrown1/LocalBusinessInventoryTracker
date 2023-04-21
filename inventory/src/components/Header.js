e7ae94308023d1344318bb7ca6ecb4f088cd4183
osbrown1 <socknot@live.com>
2023-04-21 09:50:56 -0700

    update Header with content

---
+++ inventory/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Header(){
+  return (
+    <React.Fragment>
+      <div class="header">
+        <h1>Ice Cream Shop</h1>
+        <p><em>Best Ice Cream in Town</em></p>
+      </div>
+      
+    </React.Fragment>
+  );
+}
+
+export default Header;
